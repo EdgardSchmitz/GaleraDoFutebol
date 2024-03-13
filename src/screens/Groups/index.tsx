d5e72560c6be4d70fef52830df61b6063c7a3224
Edgard Schmitz <edgardschmitz83@gmail.com>
2024-03-13 18:34:17 -0300

    add msg inicial e botões

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,20 +1,43 @@
-import { Header } from '@components/Header';
-import { Highligth } from '@components/hightlight';
-import { GroupCard } from '@components/GroupCards';
-import { Container} from './styles';
+import { useState } from 'react'
+import { FlatList } from 'react-native'
+import { Header } from '@components/Header'
+import { Highligth } from '@components/hightlight'
+import { GroupCard } from '@components/GroupCards'
+import { ListEmpty } from '@components/ListEmpty'
+import { Button } from '@components/button'
+import { Container } from './styles'
 
 export function Groups() {
-return (
+    const [groups, setGroups] = useState<string[]>([])
+
+    return (
     <Container>
-        <Header/>
-        <Highligth
-        
-        title='Turmas'
-        subtitle='Jogue com a sua turma'
+        <Header />
+            <Highligth 
+            title="Turmas" 
+            subtitle="Jogue com a sua turma" 
+            />
+            <FlatList
+                    data={groups}
+                    keyExtractor={item => item}
+                    renderItem={({ item }) => (
+            <GroupCard 
+            title={item} 
+            />
+        )}
+
+        contentContainerStyle={groups.length === 0 && {flex: 1}}
+        ListEmptyComponent={() => (
+            <ListEmpty 
+            message="Vamos cadastrar a primeira Turma?" 
+            />
+        )}
         />
 
-        <GroupCard title='Turma do Riviera' />
+        <Button
+        
+        title='Adicionar Turma'
+        />
     </Container>
-);
+    )
 }
-
